1bf96860441cacbad057c073832aa069b4421404
Mark Nicol <marknicol1991@live.co.uk>
2020-03-20 15:26:45 +0000

    Project created.

---
+++ specs/customer_spec.rb
@@ -0,0 +1,8 @@
+require('minitest/autorun')
+require('minitest/reporters')
+require_relative('../')
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+class TestCustomer < MiniTest::Test
+
+end
